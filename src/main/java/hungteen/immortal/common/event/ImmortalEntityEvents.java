19c2ec341e370c428277c1bc66db7e008849133d
HungTeen <1687289734@qq.com>
2023-03-14 18:46:34 +0800

    苦力怕傀儡

--- src/main/java/hungteen/immortal/common/event/ImmortalEntityEvents.java
+++ src/main/java/hungteen/immortal/common/event/ImmortalEntityEvents.java
@@ -1,6 +1,9 @@
 package hungteen.immortal.common.event;
 
 import hungteen.immortal.ImmortalMod;
+import net.minecraft.world.entity.ai.goal.target.NearestAttackableTargetGoal;
+import net.minecraft.world.entity.monster.Creeper;
+import net.minecraft.world.entity.raid.Raider;
 import net.minecraftforge.event.entity.EntityJoinLevelEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -15,5 +18,13 @@ public class ImmortalEntityEvents {
 
     @SubscribeEvent
     public static void onEntityJoin(EntityJoinLevelEvent event){
+        if(! event.getLevel().isClientSide()){
+            if(event.getEntity() instanceof Raider raider){
+                raider.targetSelector.addGoal(5, new NearestAttackableTargetGoal<>(raider, Creeper.class, true));
+            } else if(event.getEntity() instanceof Creeper creeper){
+                creeper.targetSelector.addGoal(3, new NearestAttackableTargetGoal<>(creeper, Raider.class, true));
+            }
+        }
     }
+
 }
--- src/main/java/hungteen/immortal/utils/EntityUtil.java
+++ src/main/java/hungteen/immortal/utils/EntityUtil.java
@@ -4,10 +4,19 @@ import hungteen.htlib.util.Triple;
 import hungteen.htlib.util.helper.ColorHelper;
 import hungteen.immortal.api.interfaces.IHasRoot;
 import hungteen.immortal.api.registry.ISpiritualType;
+import net.minecraft.advancements.CriteriaTriggers;
+import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.pattern.BlockInWorld;
+import net.minecraft.world.level.block.state.pattern.BlockPattern;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -26,6 +35,33 @@ public class EntityUtil {
 
     public static boolean isOffHolding(LivingEntity entity, Predicate<ItemStack> predicate) {
         return predicate.test(entity.getOffhandItem());
+    }
+
+    public static void multiblockSpawn(Level level, BlockPos blockPos, BlockPattern blockPattern, EntityType<?> entityType){
+        if(! level.isClientSide){
+            BlockPattern.BlockPatternMatch match = blockPattern.find(level, blockPos);
+            if (match != null) {
+                for(int i = 0; i < blockPattern.getHeight(); ++i) {
+                    BlockInWorld blockinworld = match.getBlock(0, i, 0);
+                    level.setBlock(blockinworld.getPos(), Blocks.AIR.defaultBlockState(), 2);
+                    level.levelEvent(2001, blockinworld.getPos(), Block.getId(blockinworld.getState()));
+                }
+
+                Entity entity = entityType.create(level);
+                BlockPos pos = match.getBlock(0, 2, 0).getPos();
+                entity.moveTo((double)pos.getX() + 0.5D, (double)pos.getY() + 0.05D, (double)pos.getZ() + 0.5D, 0.0F, 0.0F);
+                level.addFreshEntity(entity);
+
+                for(ServerPlayer serverplayer : level.getEntitiesOfClass(ServerPlayer.class, entity.getBoundingBox().inflate(5.0D))) {
+                    CriteriaTriggers.SUMMONED_ENTITY.trigger(serverplayer, entity);
+                }
+
+                for(int l = 0; l < blockPattern.getHeight(); ++l) {
+                    BlockInWorld world = match.getBlock(0, l, 0);
+                    level.blockUpdated(world.getPos(), Blocks.AIR);
+                }
+            }
+        }
     }
 
     public static Triple<Float, Float, Float> getRGBForSpiritual(Entity entity){
